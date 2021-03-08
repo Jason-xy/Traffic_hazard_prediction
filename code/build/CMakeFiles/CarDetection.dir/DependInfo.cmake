8c4832054c7eacd615af59763f5251674c288dc6
jason-xy <1462357003@qq.com>
2021-03-08 11:01:33 +0800

    update bin

---
+++ code/build/CMakeFiles/CarDetection.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jason/github/Traffic_hazard_prediction/code/carDetection.cpp" "/home/jason/github/Traffic_hazard_prediction/code/build/CMakeFiles/CarDetection.dir/carDetection.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_NO_DEBUG"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/cuda-10.2/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ code/build/CMakeFiles/CarDetection.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CarDetection.dir/carDetection.cpp.o"
+  "CarDetection.pdb"
+  "CarDetection"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CarDetection.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
